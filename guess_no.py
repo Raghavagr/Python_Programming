22bc980a302d81d515a098640c984edcc9f73204
raghavrg77 <52033598+agrawalassociates@users.noreply.github.com>
2020-04-16 15:33:14 +0530

    Python Guess the number
    It is a small Python Guessing a Number game

---
+++ guess_no.py
@@ -0,0 +1,16 @@
+import random
+secret = random.randrange(1,100)
+print(secret)
+guess=0
+trials=0
+while guess!=secret:
+    guess = int(input("Enter your guess:-"))
+    trials = trials + 1
+    if guess > secret:
+        print("Guess Too High")
+    elif guess < secret:
+        print("Guess Too Low")
+    else:
+        print("Yeahh! You Got it,")
+        print("Its Correct")
+print("\nThe total no. of Guess made:",trials)
